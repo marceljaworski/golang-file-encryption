fcf8c46cd9e7a5cf44a0b73440fa111d5cc3ea7f
Marcelearn <jaworskimarcel@gmail.com>
2023-11-30 18:19:37 +0100

    Decript function

--- filecrypt.go
+++ filecrypt.go
@@ -5,6 +5,7 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"crypto/sha1"
+	"encoding/hex"
 	"io"
 	"os"
 
@@ -33,7 +34,7 @@ func Encrypt(source string, password []byte) {
 	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
 		panic(err.Error())
 	}
-	// Password base key direction function, Args: key, nonce, number of iterations, lenght, algorithm
+	// Password base key direction function, Args: key password, nonce salt, number of iterations, lenght, algorithm
 	dk := pbkdf2.Key(key, nonce, 4096, 32, sha1.New)
 	block, err := aes.NewCipher(dk)
 	if err != nil {
@@ -60,6 +61,51 @@ func Encrypt(source string, password []byte) {
 	}
 }
 
-func Decrypt() {
+func Decrypt(source string, password []byte) {
+	if _, err := os.Stat(source); os.IsExist(err) {
+		panic(err.Error())
+	}
 
+	srcFile, err := os.Open(source)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer srcFile.Close()
+
+	ciphertext, err := io.ReadAll(srcFile)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	key := password
+	salt := ciphertext[len(ciphertext)-12:]
+	str := hex.EncodeToString(salt)
+	nonce, err := hex.DecodeString(str)
+
+	dk := pbkdf2.Key(key, nonce, 4096, 32, sha1.New)
+	block, err := aes.NewCipher(dk)
+	if err != nil {
+		panic(err.Error())
+	}
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	plaintext, err := aesgcm.Open(nil, nonce, ciphertext[:len(ciphertext)-12], nil)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	dstFile, err := os.Create(source)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer dstFile.Close()
+
+	_, err = dstFile.Write(plaintext)
+	if err != nil {
+		panic(err.Error())
+	}
 }
